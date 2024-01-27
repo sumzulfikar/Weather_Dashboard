c69709c15e78c492d5b375a11aa55c4917df331a
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-01-27 14:06:06 +0000

    minor css updated

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ $("#search-button").on("click",function(event){
     event.preventDefault();
 var APIKey= "166a433c57516f51dfab1f7edaed8413";
 var cityname=$("#search-input").val().trim();
-console.log(cityname);
-
-var queryURL= "https://api.openweathermap.org/data/2.5/forecast?q="+ cityname + "&appid=" + APIKey;
 //Date format expected 14/9/2022
 var currentdate=dayjs().format("DD/M/YYYY");
-console.log(currentdate);
+var queryURL= "https://api.openweathermap.org/data/2.5/forecast?q="+ cityname + "&appid=" + APIKey;
 
-$("#today").append($("<h1>").text(cityname+" ("+currentdate + " )"));
 
 
 fetch(queryURL)
@@ -19,10 +15,45 @@ return response.json();
 
 })
 .then(function(data){
-
-
-    
     console.log(data);
+var cloud=$("<i>");
+cloud.attr("class","fa-solid fa-cloud");
+
+var h1=$("<h1>");
+h1.text(`${cityname} (${currentdate})`);
+
+var locationHeader=$("#today").append(h1.append(cloud));
+
+//Temp Wind and humidity display
+var div=$("<div>");
+var ul=$("<ul>");
+div.attr("class","relevantDetails");
+var currentTemp=data.list[0].main.temp;
+currentTemp=(currentTemp-273.15).toFixed(2);
+//Tempreture in celcius
+var liTemp=$("<li>");
+liTemp=liTemp.text(`Temp: ${currentTemp} ℃`);
+ul=ul.append(liTemp);
+div=div.append(ul);
+   locationHeader.append(div);
+//Wind in kph
+var currentWind=data.list[0].wind.speed;
+currentWind=(currentWind*3.6).toFixed(2);
+var liWeather=$("<li>");
+liWeather=liWeather.text(`Wind: ${currentWind} KPH`);
+ul=ul.append(liWeather);
+div=div.append(ul);
+   locationHeader.append(div);
+
+   //Humidity %
+var currentWind=data.list[0].wind.speed;
+currentWind=(currentWind*3.6).toFixed(2);
+var liWeather=$("<li>");
+liWeather=liWeather.text(`Wind: ${currentWind} KPH`);
+ul=ul.append(liWeather);
+div=div.append(ul);
+   locationHeader.append(div);
+  
 });
 
 
